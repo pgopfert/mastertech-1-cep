db9de000302ec6ce54b5366ad8e29f69691754ee
Bruno <brunoapimentel@gmail.com>
2016-10-24 11:24:47 -0200

    Integração com webservice

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,20 @@
 angular.module('starter.controllers', [])
 
-        .controller('SearchCtrl', function ($scope) {
+        .controller('SearchCtrl', function ($scope, $http) {
+            $scope.form = {};
+
+
+            $scope.search = function(){
+                $http({
+                    method: 'GET',
+                    url: 'http://viacep.com.br/ws/' + $scope.form.cep + '/json/'
+                }).then(function (response) {
+                    $scope.address = response.data;
+                }, function (response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+            };
 
         })
 
